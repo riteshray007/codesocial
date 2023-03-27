efcdc8e36e2d52d84b4026bf6ef5ab63696af3ce
ritesh ray <ray.ritesh007@gmail.com>
2023-03-27 23:09:01 +0530

    uploaded picture preview

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -14,19 +14,40 @@ document.addEventListener('click' , (e)=>{
 })
 
 
-imginput.addEventListener('change' , fetchimg )
+// imginput.addEventListener('change' , fetchimg )
 
-function fetchimg(){
-    let file = imginput.files[0];
-    if(file){
-        const filereader = new filereader();
-        filereader.readAsDataUrl(file); 
-        filereader.addEventListener( 'load' , ()=>{
-            document.querySelector('.previewdiv').style.display = 'block';
-            document.querySelector('.previewdiv').innerHTML = ` <img class="previewimg" src='${this.result}' > `
-        } )
+// function fetchimg(){
+//     let file = imginput.files[0];
+//     if(file){
+//         // const filereader = new filereader();
+//         // filereader.readAsDataUrl(file); 
+//         // filereader.addEventListener( 'load' , ()=>{
+//         //     document.querySelector('.previewdiv').style.display = 'block';
+//         //     document.querySelector('.previewdiv').innerHTML = ` <img class="previewimg" src='${this.result}' > `
+//         // } )
+//         console.log('something ');
+//     }
+// }
+
+let prediv = $('.previewdiv')
+function readURL(input) {
+    console.log("selected some img ");
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            prediv.html('<img src="#"  class="previewpic" >')
+            prediv.css('display' , 'inline-block');
+            $('.previewpic').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
     }
 }
+
+$(".profilepic").change(function(){
+    readURL(this);
+});
 
 
 
