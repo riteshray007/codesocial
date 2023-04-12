af99dd08050e260a92c7660a418b5846d2993cad
ritesh ray <ray.ritesh007@gmail.com>
2023-04-12 19:06:10 +0530

    chat box update

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ const router = express.Router()
 const home_controller = require('../controllers/home_controller')
 
 
-router.get('/' , home_controller.home);
+router.use('/' , require('./posts'));
 router.get('/about' , home_controller.about);
- router.use('/posts' , require('./posts') );
+ router.use('/posts' ,  require('./posts'));
 router.use('/users' , require('./users') );
 router.use('/likes' , require('./likes'));
 
