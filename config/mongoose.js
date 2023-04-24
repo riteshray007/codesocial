3cb70c233ebfc44df8733c25d1e4e648c1856d84
ritesh ray <ray.ritesh007@gmail.com>
2023-04-24 14:06:42 +0530

    mongo

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const env = require('./environment');
 
-mongoose.connect(`mongodb://0.0.0.0:27017
-/${env.db}`);
+mongoose.connect(`mongodb://0.0.0.0:27017/${env.db}`);
 
 const db = mongoose.connection
 
